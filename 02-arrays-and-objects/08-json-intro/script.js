7c66e009b7a65306d3a16d339f175dc2db2ca3ed
Aziz Rakhim <azizbekrahimjonov80@gmail.com>
2023-11-27 00:18:18 +0500

    feat: finished lesson 2.6, 2.7 and 2.8

---
+++ 02-arrays-and-objects/08-json-intro/script.js
@@ -0,0 +1,28 @@
+const post = {
+  id: 1,
+  title: "Post One",
+  body: "This is the body",
+};
+
+// Convert to JSON string
+const str = JSON.stringify(post);
+
+// Parse JSON
+const obj = JSON.parse(str);
+
+const posts = [
+  {
+    id: 1,
+    title: "Post One",
+    body: "This is the body",
+  },
+  {
+    id: 2,
+    title: "Post Two",
+    body: "This is the body",
+  },
+];
+
+const str2 = JSON.stringify(posts);
+
+console.log(str2);
