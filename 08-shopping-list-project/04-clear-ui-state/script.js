79518621b7143c5eeae9040ef870fff22fbaac45
Aziz Rakhim <azizrakhim17@gmail.com>
2024-07-12 17:18:04 +0500

    lesson 8.4: clear ui state

---
+++ 08-shopping-list-project/04-clear-ui-state/script.js
@@ -0,0 +1,86 @@
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
+// const items = itemList.querySelectorAll("li"); This is already defined. When page loads, it is gonna be empty. Hence, even though we add item through form, it is gonna stay empty 'cause it is already defined. That's why our checkUI func will not work. We need to call this inside the func to get newly defined value each time
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  // Validate Input
+  if (newItem === "") {
+    alert("Please add an item");
+
+    return;
+  }
+
+  // Create list item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  // Add li to the DOM
+  itemList.appendChild(li);
+
+  checkUI();
+
+  itemInput.value = "";
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+
+  return icon;
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+
+      checkUI();
+    }
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  // console.log(items);
+
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+// Event Listeners
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
+
+checkUI();
