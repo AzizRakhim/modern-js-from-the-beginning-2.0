629ff115dc5b87a2aeb3d7eac8e2e2cf0dd89f2d
Aziz Rakhim <azizrakhim17@gmail.com>
2024-07-12 18:12:30 +0500

    lesson 8.6: local storage crash course

---
+++ 08-shopping-list-project/06-local-storage-crash-course/script.js
@@ -0,0 +1,108 @@
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  // Validate Input
+  if (newItem === "") {
+    alert("Please add an item");
+
+    return;
+  }
+
+  // Create list item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  // Add li to the DOM
+  itemList.appendChild(li);
+
+  checkUI();
+
+  itemInput.value = "";
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+
+  return icon;
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+
+      checkUI();
+    }
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  checkUI();
+}
+
+// His version
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+// Event Listeners
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
+
+checkUI();
+
+// LocalStorage and SessionStorage, they both have the same methods. The difference is that localStorage is permanent unless we specify it to be terminated whilst sessionStorage is cleared the page is closed.
+
+localStorage.setItem("name", "Brad");
+console.log(localStorage.getItem("name"));
+// localStorage.removeItem("name");
+localStorage.clear();
