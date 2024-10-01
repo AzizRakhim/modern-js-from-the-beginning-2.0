e680791d42143d124887b3f1c4786fd42812cf70
Aziz Rakhim <azizrakhim17@gmail.com>
2024-10-01 11:48:42 +0500

    Lesson 09.07: Promises finished

---
+++ 09-asynchronous-javascript/07-promises/script.js
@@ -0,0 +1,50 @@
+// Create a promise
+const promise = new Promise((resolve, reject) => {
+  // Do some async task
+  setTimeout(() => {
+    console.log("Async task complete");
+    resolve();
+  }, 1000);
+});
+
+// promise.then(() => {
+//   console.log("Promise consumed..");
+// });
+
+// new Promise((resolve, reject) => {
+//   // Do some async task
+//   setTimeout(() => {
+//     // console.log("Async task 2 complete");
+//     resolve({ name: "John", age: 30 });
+//   }, 1000);
+// }).then((user) => {
+//   // console.log("promise 2 consumed");
+//   console.log(user);
+// });
+
+const getUser = new Promise((resolve, reject) => {
+  // Do some async task
+  setTimeout(() => {
+    // console.log("Async task 2 complete");
+
+    let error = true;
+
+    if (!error) {
+      resolve({ name: "John", age: 30 });
+    } else {
+      reject("Error: Something went wrong");
+    }
+  }, 1000);
+});
+
+getUser
+  .then((user) => {
+    // console.log("promise 2 consumed");
+    console.log(user);
+  })
+  .catch((error) => console.log(error))
+  .finally(() => {
+    console.log("The promise has been resolved or rejected");
+  });
+
+console.log("Hello from global scope");
