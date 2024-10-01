7d8d60d4046a681cc74dc0ada5c22564935d0de0
Aziz Rakhim <azizrakhim17@gmail.com>
2024-10-01 14:23:46 +0500

    Lesson 09.10: Promises vs callback hell finished

---
+++ 09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
@@ -0,0 +1,62 @@
+// function getData(endpoint, cb) {
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.open('GET', endpoint);
+
+//   xhr.onreadystatechange = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//       cb(JSON.parse(this.responseText));
+//     }
+//   };
+
+//   setTimeout(() => {
+//     xhr.send();
+//   }, Math.floor(Math.random() * 3000) + 1000);
+// }
+
+// getData('./movies.json', (data) => {
+//   console.log(data);
+//   getData('./actors.json', (data) => {
+//     console.log(data);
+//     getData('./directors.json', (data) => {
+//       console.log(data);
+//     });
+//   });
+// });
+
+function getData(endpoint) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", endpoint);
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject("Something went wrong");
+        }
+      }
+    };
+
+    setTimeout(() => {
+      xhr.send();
+    }, Math.floor(Math.random() * 3000) + 1000);
+  });
+}
+
+getData("./movies.json")
+  .then((movies) => {
+    console.log(movies);
+    return getData("./actors.json");
+  })
+  .then((actors) => {
+    console.log(actors);
+    // return getData("./directors1.json"); Triggers catch error
+    return getData("./directors.json");
+  })
+  .then((directors) => {
+    console.log(directors);
+  })
+  .catch((error) => console.log(error));
