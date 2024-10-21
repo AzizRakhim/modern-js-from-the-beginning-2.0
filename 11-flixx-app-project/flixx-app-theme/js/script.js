73ad10f413490097e87a02f58bdea278b8dea3be
Aziz Rakhim <azizrakhim17@gmail.com>
2024-10-21 15:48:13 +0500

    Lesson-11: Flixx - TV Show Details Page

--- 11-flixx-app-project/flixx-app-theme/js/script.js
+++ 11-flixx-app-project/flixx-app-theme/js/script.js
@@ -181,6 +181,80 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Display Show Details
+async function displayShowDetails() {
+  const showId = window.location.search.split("=")[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overlay for background image
+  displayBackgroundImage("tv", show.backdrop_path);
+
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+         ${
+           show.poster_path
+             ? `
+                <img
+                  src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                  class="card-img-top"
+                  alt="${show.name}"
+                />
+              `
+             : `
+                <img
+                  src="images/no-image.jpg"
+                  class="card-img-top"
+                  alt="${show.name}"
+                />
+              `
+         }
+      </div>
+      <div>
+        <h2>${show.name}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${show.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+        <p>
+          ${show.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${show.genres.map((genre) => `<li>${genre.name}.</li>`).join("")}
+        </ul>
+        <a href="${
+          show.homepage
+        }" target="_blank" class="btn">Visit Show Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Show Info</h2>
+      <ul>
+        <li><span class="text-secondary">Number of Episodes:</span> ${
+          show.number_of_episodes
+        }</li>
+        <li><span class="text-secondary">Last Episode To Air:</span> ${
+          show.last_episode_to_air.name
+        }</li>
+        <li><span class="text-secondary">Status:</span> ${show.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">
+        ${show.production_companies
+          .map((company) => `<span>${company.name}</span>`)
+          .join(", ")}
+      </div>
+    </div>
+  `;
+
+  document.querySelector("#show-details").appendChild(div);
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   // Register your key at https://www.themoviedb.org/settings/api and enter here
@@ -247,7 +321,7 @@ function init() {
       displayMovieDetails();
       break;
     case "/11-flixx-app-project/flixx-app-theme/tv-details.html":
-      console.log("TV Details");
+      displayShowDetails();
       break;
     case "/11-flixx-app-project/flixx-app-theme/search.html":
       console.log("Search");
