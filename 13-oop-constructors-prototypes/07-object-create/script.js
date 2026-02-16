b07a2d0807b44db66caf632d50bdbd0678c9e623
Aziz <azizrakhim17@gmail.com>
2026-02-16 11:45:42 +0500

    lesson-13.07: object create

---
+++ 13-oop-constructors-prototypes/07-object-create/script.js
@@ -0,0 +1,26 @@
+const rectanglePrototypes = {
+  area: function () {
+    return this.width * this.height;
+  },
+  perimeter: function () {
+    return 2 * (this.width + this.height);
+  },
+  isSquare: function () {
+    return this.width === this.height;
+  },
+};
+
+function createRectangle(width, height) {
+  return Object.create(rectanglePrototypes, {
+    width: { value: width },
+    height: { value: height },
+  });
+}
+
+const rect = createRectangle(10, 20);
+console.log(rect);
+console.log(rect.area());
+console.log(rect.isSquare());
+
+const rect2 = createRectangle(20, 20);
+console.log(rect2.area());
