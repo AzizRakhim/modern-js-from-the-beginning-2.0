b4240d1b5092895fe3b1ef3ff0c5685b87ae4411
Aziz <azizrakhim17@gmail.com>
2026-04-13 14:54:03 +0500

    lesson-14.05: getters setters defineproperty

---
+++ 14-oop-classes-private-properties/05-getters-setters-defineproperty/script.js
@@ -0,0 +1,71 @@
+// Constructor Function
+function Person(firstName, lastName) {
+  this._firstName = firstName;
+  this._lastName = lastName;
+
+  Object.defineProperty(this, "firstName", {
+    get: function () {
+      return this.capitalizeFirst(this._firstName);
+    },
+    set: function (value) {
+      this._firstName = value;
+    },
+  });
+
+  Object.defineProperty(this, "lastName", {
+    get: function () {
+      return this.capitalizeFirst(this._lastName);
+    },
+    set: function (value) {
+      this._lastName = value;
+    },
+  });
+
+  Object.defineProperty(this, "fullName", {
+    get: function () {
+      return this.firstName + " " + this.lastName;
+    },
+  });
+}
+
+Person.prototype.capitalizeFirst = function (value) {
+  return value?.charAt(0)?.toUpperCase() + value?.slice(1);
+};
+
+// Object Literal
+const personObj = {
+  _firstName: "jane",
+  _lastName: "doe",
+
+  get firstName() {
+    return Person.prototype.capitalizeFirst(this._firstName);
+  },
+
+  set firstName(value) {
+    this._lastName = value;
+  },
+
+  get lastName() {
+    return Person.prototype.capitalizeFirst(this._lastName);
+  },
+
+  set firstName(value) {
+    this._lastName = value;
+  },
+
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+const person1 = new Person("john", "doe");
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.fullName);
+
+const person2 = Object.create(personObj);
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.fullName);
+
+console.log({ person1, personObj });
