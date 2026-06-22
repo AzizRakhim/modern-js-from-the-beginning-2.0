45f53c00641f8caa8d7949de340388c74bd58b44
Aziz Rakhim <azizrakhim17@gmail.com>
2026-06-22 17:27:18 +0500

    lesson-17-03: generators

---
+++ 17-iterators-generators-data-structures/03-generators/script.js
@@ -0,0 +1,25 @@
+function* createTeamIterator(teams) {
+  for (let i = 0; i < teams.length; i++) {
+    yield teams[i];
+  }
+}
+
+const teams = ["Red Sox", "Yankees", "Astros", "Dodgers"];
+
+const iterator = createTeamIterator(teams);
+
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+
+// for (let team of createTeamIterator(teams)) {
+//   console.log(team);
+// }
+
+// console.log([...createTeamIterator(teams)]);
+
+const [first, second, third] = createTeamIterator(teams);
+
+console.log(first, second, third);
